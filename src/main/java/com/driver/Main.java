865368dec46e1e5fdb44a735204a5b46c61e566e
mehta <mehta@LAPTOP-B1LKLJTG>
2023-09-27 17:11:52 +0530

    Second Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,23 @@ package com.driver;
 
 public class Main {
 	
+	public static class A {
+		public String meth() {
+			return "Invoking method froom class A";
+		}
+	}
+	
+	public static class B extends A {
+
+		@Override
+		public String meth() {
+			// TODO Auto-generated method stub
+			return "Method is overridden in Extended class B";
+		}
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		B b = new B();
 		System.out.println(b.meth());
